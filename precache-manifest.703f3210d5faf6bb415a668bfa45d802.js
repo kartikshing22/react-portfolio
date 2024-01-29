b58da3071039dd7189056b47e52d84023c5ca16b
kartikshing22 <78463806+kartikshing22@users.noreply.github.com>
2024-01-29 23:37:28 +0530

    Updates

---
+++ precache-manifest.703f3210d5faf6bb415a668bfa45d802.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f74b754a2a8d9a25df98b56e4cd75f1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "330295af44af5fbe469e",
+    "url": "/react-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "94c51211882a7806d879",
+    "url": "/react-portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "330295af44af5fbe469e",
+    "url": "/react-portfolio/static/js/2.2d23e445.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-portfolio/static/js/2.2d23e445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c51211882a7806d879",
+    "url": "/react-portfolio/static/js/main.3bef140e.chunk.js"
+  },
+  {
+    "revision": "6f6086f91a7ae7353c00",
+    "url": "/react-portfolio/static/js/runtime-main.873154d3.js"
+  }
+]);
